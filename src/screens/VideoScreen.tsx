0247398d5eceb4739556504624134a0ed552ca3f
aschx-dev <aschalewmm93@gmail.com>
2025-08-27 18:07:26 +0300

    add video to exrcise

--- src/screens/VideoScreen.tsx
+++ src/screens/VideoScreen.tsx
@@ -13,28 +13,27 @@ export default function VideoScreen({ route }: Props) {
   const { url, exerciseId } = route.params;
   const exercise = exerciseId ? getExerciseById(exerciseId) : undefined;
 
-  if (exercise && exercise.localVideo) {
-    return (
-      <SafeAreaView style={styles.safe}>
-        <View style={styles.playerBox}>
-          <Video
-            source={exercise.localVideo}
-            style={styles.player}
-            useNativeControls
-            resizeMode={ResizeMode.CONTAIN}
-          />
-        </View>
-      </SafeAreaView>
-    );
-  }
   return (
     <SafeAreaView style={styles.safe}>
-      <View style={styles.playerBox}>
-        <WebView
-          source={{ uri: url }}
-          style={styles.player}
-          allowsFullscreenVideo
-        />
+      <View style={styles.container}>
+        <View style={styles.playerBox}>
+          {exercise && exercise.localVideo ? (
+            <Video
+              source={exercise.localVideo}
+              style={styles.player}
+              useNativeControls
+              resizeMode={ResizeMode.CONTAIN}
+            />
+          ) : (
+            <WebView
+              source={{ uri: url }}
+              style={styles.player}
+              allowsFullscreenVideo
+              mediaPlaybackRequiresUserAction={false}
+              allowsInlineMediaPlayback
+            />
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -42,15 +41,22 @@ export default function VideoScreen({ route }: Props) {
 
 const styles = StyleSheet.create({
   safe: { flex: 1, backgroundColor: "#000" },
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
   playerBox: {
-    alignSelf: "center",
     width: "100%",
     aspectRatio: 16 / 9,
-    backgroundColor: "#000",
-    overflow: "hidden",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center",
   },
   player: {
-    width: "100%",
-    height: "100%",
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   },
 });
